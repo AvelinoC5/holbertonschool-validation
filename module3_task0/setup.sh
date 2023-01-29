d4ddf5b727fa5b586ecf45d606672169d4e94874
AvelinoC5 <avelino_63@hotmail.com>
2023-01-29 12:46:11 -0500

    update

--- module3_task0/setup.sh
+++ module3_task0/setup.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env bash
-
-apt-get update && apt-get install -y curl make
-
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo_extended.deb
-apt-get install -y ./hugo_extended.deb
-rm hugo_extended.deb
-
+#!/bin/bash
+apt-get update && apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
 make build
